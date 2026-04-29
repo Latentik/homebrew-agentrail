98fd76afc1623d705023a2c3f22b4999e3e6a174
Dariusz Wójcik <dariusz.wojcik@netrix.com.pl>
2026-04-29 22:16:41 +0200

    chore: align formula to arm64 release

--- Formula/agentrail.rb
+++ Formula/agentrail.rb
@@ -1,17 +1,12 @@
 class Agentrail < Formula
   desc "Portable coding-agent handoff CLI"
   homepage "https://github.com/Latentik/agentrail"
-  version "0.1.0"
+  version "0.1.1"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/Latentik/agentrail/releases/download/v0.1.0/agentrail-v0.1.0-macos-arm64.tar.gz"
-      sha256 "REPLACE_WITH_RELEASE_SHA256"
-    end
-
-    on_intel do
-      url "https://github.com/Latentik/agentrail/releases/download/v0.1.0/agentrail-v0.1.0-macos-x86_64.tar.gz"
+      url "https://github.com/Latentik/agentrail/releases/download/v0.1.1/agentrail-v0.1.1-macos-arm64.tar.gz"
       sha256 "REPLACE_WITH_RELEASE_SHA256"
     end
   end
