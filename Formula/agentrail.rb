62ddf46485d45dad4302996ac51333944ea49668
Dariusz Wójcik <dariusz.wojcik@netrix.com.pl>
2026-04-29 21:35:12 +0200

    Initialize agentrail Homebrew tap

---
+++ Formula/agentrail.rb
@@ -0,0 +1,26 @@
+class Agentrail < Formula
+  desc "Portable coding-agent handoff CLI"
+  homepage "https://github.com/Latentik/agentrail"
+  version "0.1.0"
+  license "Apache-2.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/Latentik/agentrail/releases/download/v0.1.0/agentrail-v0.1.0-macos-arm64.tar.gz"
+      sha256 "REPLACE_WITH_RELEASE_SHA256"
+    end
+
+    on_intel do
+      url "https://github.com/Latentik/agentrail/releases/download/v0.1.0/agentrail-v0.1.0-macos-x86_64.tar.gz"
+      sha256 "REPLACE_WITH_RELEASE_SHA256"
+    end
+  end
+
+  def install
+    bin.install "agentrail"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/agentrail --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# homebrew-agentrail
+
+Homebrew tap for `agentrail`.
+
+## Install
+
+```bash
+brew tap Latentik/agentrail
+brew install agentrail
+```
+
+## How it is updated
+
+This repository is updated automatically by the release workflow in `Latentik/agentrail`.
+Tagged releases in the main project publish macOS binaries and then update `Formula/agentrail.rb` here.
